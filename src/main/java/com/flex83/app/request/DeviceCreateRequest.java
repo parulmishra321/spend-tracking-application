e3aa162c51660108960e338359e46c0cb16adec3
iot83 <parul.mishra@iot83.com>
2022-02-11 20:54:04 +0530

    FC-58

--- src/main/java/com/flex83/app/request/DeviceCreateRequest.java
+++ src/main/java/com/flex83/app/request/DeviceCreateRequest.java
@@ -1,11 +1,13 @@
 package com.flex83.app.request;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 public class DeviceCreateRequest {
     @NotBlank(message = "pattern.ConnectorReq.name")
     @Pattern(regexp="^[a-zA-Z0-9]([\\w]*[a-zA-Z0-9]?$)",message="pattern.ConnectorReq.name")
+    @NotNull(message = "pattern.ConnectorReq.name")
     private String name;
     private String description;
     private String deviceTypeId;
