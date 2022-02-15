456cef071e3ae021fc7bfb1f06015ea398eee888
ayuu01 <ayuagrawal180@gmail.com>
2022-02-15 17:50:42 +0530

    fc-69

--- src/main/java/com/flex83/app/controller/DatabaseConfigController.java
+++ src/main/java/com/flex83/app/controller/DatabaseConfigController.java
@@ -3,16 +3,14 @@ package com.flex83.app.controller;
 import com.flex83.app.bussiness.DatabaseConfigurationService;
 import com.flex83.app.enums.ApiResponseCode;
 import com.flex83.app.request.DatabaseConfigRequest;
+import com.flex83.app.request.DatabaseConfigUpdateRequest;
 import com.flex83.app.response.ApiResponseDTO;
 import com.flex83.app.response.generic.AccessDeniedResponseDTO;
 import com.flex83.app.response.generic.BadRequestResponseDTO;
 import com.flex83.app.response.generic.NotAuthenticatedResponseDTO;
 import com.flex83.app.response.generic.ResponseDTO;
 import com.flex83.app.response.utils.ResponseUtil;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +24,7 @@ public class DatabaseConfigController {
     @Autowired
     private DatabaseConfigurationService databaseConfigurationService;
 
-    @ApiOperation(value = "Create/update database Configs")
+    @ApiOperation(value = "Create/create database Configs")
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "Created", response = ApiResponseDTO.class),
             @ApiResponse(code = 400, message = "Bad Request", response = BadRequestResponseDTO.class),
@@ -38,6 +36,61 @@ public class DatabaseConfigController {
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseDTO<?> createDatabaseConfig(@RequestBody DatabaseConfigRequest databaseConfigRequest) throws Exception {
         databaseConfigurationService.createDataBaseConfig(databaseConfigRequest);
+        return responseUtil.ok(ApiResponseCode.SUCCESS);
+    }
+
+    @ApiOperation(value = "Get all database Configs ")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created", response = ApiResponseDTO.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = BadRequestResponseDTO.class),
+            @ApiResponse(code = 401, message = "You are Not Authenticated", response = NotAuthenticatedResponseDTO.class),
+            @ApiResponse(code = 403, message = "Not Authorized on this resource", response = AccessDeniedResponseDTO.class),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+    })
+    @GetMapping
+    public ResponseDTO<?> getDatabaseConfig() throws Exception {
+        return responseUtil.ok(databaseConfigurationService.getDataBaseConfig(), ApiResponseCode.SUCCESS);
+    }
+
+    @ApiOperation(value = "Get database Configs by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created", response = ApiResponseDTO.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = BadRequestResponseDTO.class),
+            @ApiResponse(code = 401, message = "You are Not Authenticated", response = NotAuthenticatedResponseDTO.class),
+            @ApiResponse(code = 403, message = "Not Authorized on this resource", response = AccessDeniedResponseDTO.class),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+    })
+    @GetMapping("/{id}")
+    public ResponseDTO<?> getDatabaseConfigById(@ApiParam(value = "id",required = true) @PathVariable String id) throws Exception {
+        return responseUtil.ok(databaseConfigurationService.getDataBaseConfigById(id), ApiResponseCode.SUCCESS);
+    }
+
+
+    @ApiOperation(value = "Delete database Configs")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created", response = ApiResponseDTO.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = BadRequestResponseDTO.class),
+            @ApiResponse(code = 401, message = "You are Not Authenticated", response = NotAuthenticatedResponseDTO.class),
+            @ApiResponse(code = 403, message = "Not Authorized on this resource", response = AccessDeniedResponseDTO.class),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+    })
+    @DeleteMapping("/{id}")
+    public ResponseDTO<?> deleteDatabaseConfig(@ApiParam(value = "id",required = true) @PathVariable String id) throws Exception {
+        databaseConfigurationService.deleteByIdDataBaseConfig(id);
+        return responseUtil.ok(ApiResponseCode.SUCCESS);
+    }
+
+    @ApiOperation(value = "Update database Configs")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created", response = ApiResponseDTO.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = BadRequestResponseDTO.class),
+            @ApiResponse(code = 401, message = "You are Not Authenticated", response = NotAuthenticatedResponseDTO.class),
+            @ApiResponse(code = 403, message = "Not Authorized on this resource", response = AccessDeniedResponseDTO.class),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+    })
+    @PutMapping("/{id}")
+    public ResponseDTO<?> updateDatabaseConfig(@ApiParam(value = "databaseConfigUpdateRequest", required = true) @RequestBody DatabaseConfigUpdateRequest databaseConfigUpdateRequest, @ApiParam(value = "id",required = true) @PathVariable("id") String id) throws Exception {
+        databaseConfigurationService.updateDataBaseConfig(databaseConfigUpdateRequest,id);
         return responseUtil.ok(ApiResponseCode.SUCCESS);
     }
 }
---
+++ src/main/java/com/flex83/app/request/DatabaseConfigUpdateRequest.java
@@ -0,0 +1,49 @@
+package com.flex83.app.request;
+
+public class DatabaseConfigUpdateRequest {
+    private String server;
+    private String port;
+    private String databaseName;
+    private String username;
+    private String password;
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
